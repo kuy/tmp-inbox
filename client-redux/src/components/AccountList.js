864e4726973643243540c0a4f16aad5269db5c96
Yuki Kodama <endflow.net@gmail.com>
2015-09-11 11:19:46 +0900

    Re-implement AccountList with Material-UI
    Awesome, it's great!

--- client-redux/src/components/AccountList.js
+++ client-redux/src/components/AccountList.js
@@ -1,25 +1,27 @@
 import classnames from 'classnames';
 import React, { Component, PropTypes } from 'react';
+import { List, ListItem, Styles } from 'material-ui';
 
 export default class AccountList extends Component {
+  getChildContext() {
+    return {
+      muiTheme: (new Styles.ThemeManager())
+    };
+  }
+
   render() {
     const { onSelect } = this.props;
     const { accounts, selectedAccount } = this.props;
 
     return (
-      <div className="pure-menu account-list pure-u-1-5">
-        <span className="pure-menu-heading">Accounts</span>
-        <ul className="pure-menu-list">
+      <div className="pure-u-1-5">
+        <List subheader="Accounts">
           {
             accounts.map(account =>
-              <li className={classnames({
-                'pure-menu-item': true,
-                'selected': account === selectedAccount
-              })} key={account}>
-                <a href="#account" className="pure-menu-link" onClick={() => onSelect(account)}>{account}</a>
-              </li>)
+              <ListItem primaryText={account} />
+            )
           }
-        </ul>
+        </List>
       </div>
     );
   }
@@ -29,4 +31,8 @@ AccountList.propTypes = {
   selectedAccount: PropTypes.string,
   accounts: PropTypes.arrayOf(PropTypes.string).isRequired,
   onSelect: PropTypes.func.isRequired
+};
+
+AccountList.childContextTypes = {
+  muiTheme: PropTypes.object
 };
